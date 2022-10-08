a3b4ba19006f37a0e98d316fced5a8b5415d5e90
shendy68 <shendy68@gmail.com>
2022-10-08 20:34:14 +0400

    update cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,5 +2,9 @@ import prompt
 
 
 def welcome_user():
+    global name
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
+
+if __name__ == '__main__':
+    welcome_user()
