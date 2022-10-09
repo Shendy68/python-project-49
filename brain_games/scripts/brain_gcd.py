8214a1bd45f211a7e6e4b23e0eff5d013512154d
shendy68 <shendy68@gmail.com>
2022-10-09 20:57:40 +0400

    update brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -21,7 +21,7 @@ def gcd_game():
         if str(quest) == ans:
             print('Correct!')
             if i == 3:
-                print(f'Congratulations, {name}')
+                print(f'Congratulations, {name}!')
         else:
             print(f"'{ans}' is wrong answer ;(. Correct answer was '{quest}'.")
             print(f"Let's try again, {name}!")
