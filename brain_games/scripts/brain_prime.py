1cb36ed011433b6ae88ed41e6525b4d7d3c23267
shendy68 <shendy68@gmail.com>
2022-10-09 16:16:28 +0400

    add brain_prime.py

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import prompt
+from random import randint
+
+
+def welcome_user():
+    global name
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+
+
+def prime_num():
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    i = 1
+    while i <= 3:
+        num = randint(0, 99)
+        print(f'Question: {num}')
+        ans = prompt.string('Your answer: ')
+        for div in range(2, num):
+            if num % div == 0:
+                quest = 'no'
+                break
+            else:
+                quest = 'yes'
+        if str(quest) == ans:
+            print('Correct!')
+            if i == 3:
+                print(f'Congratulations, {name}')
+        else:
+            print(f"'{ans}' is wrong answer ;(. Correct answer was '{quest}'.")
+            print(f"Let's try again, {name}!")
+        i += 1
+
+
+def main():
+    print('Welcom to the Brain Games!')
+    welcome_user()
+    prime_num()
+
+
+if __name__ == '__main__':
+    main()
