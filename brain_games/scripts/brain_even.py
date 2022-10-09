9281480a491dcdfa22c86a299aeba0390d130b45
shendy68 <shendy68@gmail.com>
2022-10-09 20:57:19 +0400

    update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -23,7 +23,7 @@ def even():
         if str(quest) == ans:
             print('Correct!')
             if i == 3:
-                print(f'Congratulations, {name}')
+                print(f'Congratulations, {name}!')
         else:
             print(f"'{ans}' is wrong answer ;(. Correct answer was '{quest}'.")
             print(f"Let's try again, {name}!")
