e3fa1c2eff9bb91fcfa2abc4e7883a78db3b6e83
shendy68 <shendy68@gmail.com>
2022-10-09 20:31:00 +0400

    update brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,10 +4,8 @@ from brain_games.cli import welcome_user
 
 def main():
     print('Welcom to the Brain Games!')
+    welcome_user()
 
 
 if __name__ == '__main__':
     main()
-    welcome_user()
-
-
